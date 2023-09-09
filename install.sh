81f33c1696d288f97a2656c0d0131f7fe2db41c8
Riliumph <riliumph@outlook.jp>
2023-09-10 02:15:44 +0900

    [update] install script
    init submodule when submodule not inited

--- install.sh
+++ install.sh
@@ -20,7 +20,48 @@ abs_dirname()
   pwd -P # return string
 }
 
+inited_submodule()
+{
+  local target_path=$1
+  files=("${target_path}/*")
+  if (("${#files}" != 0)); then
+    # echo "${target_path} is inited"
+    return 0
+  fi
+  # echo "${target_path} is not inited"
+  return 1
+}
+
+all()
+{
+  local func="$1"
+  shift # 引数を一つズラす
+  local data=("$@")
+  for d in "${data[@]}"; do
+    echo "checking ${d} ..."
+    if ! "$func" "$d"; then
+      return 1
+    fi
+  done
+  return 0
+}
+
 root=$(abs_dirname "$0")
+# echo "root is ${root}"
+
+echo "check submodules ..."
+mapfile -t dirs < <(find "${root}" -maxdepth 1 -mindepth 1 -type d \( ! -name .git -a ! -name .vscode \))
+# echo "submodules: ${dirs[*]}"
+
+if ! (all "inited_submodule" "${dirs[@]}"); then
+  echo "detected noninited submodule"
+  echo "init submodule ..."
+  git submodule init
+  git submodule update
+fi
+
+echo "install submodules"
+
 # install bash config
 cd "${root}/bash" || exit
 ./INSTALL.bash
